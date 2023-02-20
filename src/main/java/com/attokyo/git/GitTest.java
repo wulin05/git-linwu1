040ebfb74d51204d574edee7593cd04dabb8085b
wulin05 <wulin05@yeah.net>
2023-02-20 14:02:25 +0900

    first commit

---
+++ src/main/java/com/attokyo/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.attokyo.git;
+
+import java.io.PrintStream;
+
+/**
+ * @author linwu
+ */
+public class GitTest {
+    public static void  main(String[] args) {
+        System.out.printf("hello git , I'm linwu");
+    }
+}
