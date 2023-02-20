63cc34541b45ed8ab6a75a129649e5129047bd3f
wulin05 <wulin05@yeah.net>
2023-02-20 14:02:25 +0900

    second commit

---
+++ src/main/java/com/attokyo/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.attokyo.git;
+
+/**
+ * @author linwu
+ */
+public class GitTest {
+    public static void  main(String[] args) {
+        System.out.println("hello git , I'm linwu-1");
+        System.out.println("hello git , I'm linwu-2");
+    }
+}
