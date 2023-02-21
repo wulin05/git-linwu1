04b844e04f9e06d9c60403379b9394b650913d52
wulin05 <wulin05@yeah.net>
2023-02-21 17:05:59 +0900

    17:05 commit

--- src/main/java/com/attokyo/git/GitTest.java
+++ src/main/java/com/attokyo/git/GitTest.java
@@ -1,22 +1,12 @@
 package com.attokyo.git;
-<<<<<<< HEAD
-
-=======
->>>>>>> hot-fix
 /**
  * @author linwu
  */
 public class GitTest {
-<<<<<<< HEAD
-    public static void  main(String[] args) {
-        System.out.println("hello git , I'm linwu-1");
-        System.out.println("hello git , I'm linwu-2");
-=======
     public static void main(String[] args) {
         System.out.println("hello git1,I'm linwu");
         System.out.println("hello git2,I'm linwu");
         System.out.println("hello git3,I'm linwu");
         System.out.println("hello git4,I'm linwu");
->>>>>>> hot-fix
     }
 }
